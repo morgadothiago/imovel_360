7d9d983892e526e8dfe44f407e220b619145eda7
thiago morgado <thiago_morgado@hotmail.com>
2025-04-15 20:29:45 -0300

    atualizando repositorio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,6 +43,65 @@ export default function Home() {
             </div>
           </div>
         </div>
+
+        <div className="relative w-full max-w-1xl overflow-hidden ">
+          <div className="bg-white sm:p-6 rounded-lg  md:w-[80%] p-3">
+            <h1 className="text-[#087B82] text-2xl md:text-3xl font-bold mb-4">
+              Quais são os benefícios:
+            </h1>
+            <div className="px-4">
+              <ol className="list-decimal pl-5 mb-6">
+                <li>
+                  <strong>Segurança Jurídica</strong>
+                  <ul>
+                    <li>
+                      Prova de propriedade: A regularização confirma a posse
+                      legal do imóvel, evitando disputas judiciais.
+                    </li>
+                    <li>
+                      Proteção contra reinvindicações: Reduz o risco de perder o
+                      imóvel para terceiros que aleguem direitos sobre a
+                      propriedade.
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <strong>Valorização do Imóvel</strong>
+                  <ul>
+                    <li>
+                      Imóveis regularizados têm maior valor de mercado, pois são
+                      mais atraentes para compradores e investidores.
+                    </li>
+                    <li>
+                      Bancos e financeiras só concedem crédito (financiamento,
+                      hipoteca) para imóveis regularizados.
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <strong>Possibilidade de Venda Financiada pelo Banco</strong>
+                  <ul>
+                    <li>
+                      Sem a documentação em dia, fica difícil vender o imóvel,
+                      pois os bancos não financiam o imóvel sem a matrícula
+                      regular.
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <strong>Evita Multas e Ações Judiciais</strong>
+                  <ul>
+                    <li>
+                      Imóveis irregulares podem ser alvo de fiscalização,
+                      embargo ou até demolição (especialmente em áreas de
+                      preservação ou risco).
+                    </li>
+                  </ul>
+                </li>
+              </ol>
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   );
